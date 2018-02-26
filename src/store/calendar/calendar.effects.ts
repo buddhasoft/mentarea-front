4a2d2c4a2cfb1b80c12d2365a4a98cf2cf6a7aff
v.mozharovsky <v.mozharovsky@gmail.com>
2018-02-26 17:59:29 +0200

    constants for gapi added

--- src/store/calendar/calendar.effects.ts
+++ src/store/calendar/calendar.effects.ts
@@ -9,6 +9,15 @@ import {GoogleApiService} from "ng-gapi"
 import {Store} from "@ngrx/store"
 import {FetchEvents, FetchEventsSuccess, InitCalendarSuccess} from "./calendar.actions"
 import {Observable} from "rxjs/Observable"
+import {environment} from "../../environments/environment"
+
+const CLIENT_ID = environment.production
+  ?  '57344781856-5g0quuin3l845gmtjbepllpg7mir6eef.apps.googleusercontent.com'
+  :  '57344781856-79hcun89s3lsaimo8086e9pqmgo4uavv.apps.googleusercontent.com'
+
+const API_KEY = environment.production
+  ?  'AIzaSyBlm-TsqAHnsazKfouXDWf8RoMjYq--AUI'
+  :  'AIzaSyDGe0IAMJilnIpQYapviFBjO8rQppho3mA'
 
 @Injectable()
 export class CalendarEffects{
@@ -47,8 +56,8 @@ export class CalendarEffects{
 
   initClient() {
     return gapi.client.init({
-      apiKey: 'AIzaSyDGe0IAMJilnIpQYapviFBjO8rQppho3mA',
-      clientId: '57344781856-79hcun89s3lsaimo8086e9pqmgo4uavv.apps.googleusercontent.com',
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
       discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
       scope: "https://www.googleapis.com/auth/calendar"
     }).then(() => this.store.dispatch(new InitCalendarSuccess()))
