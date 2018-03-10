1320d867f178cb963330cb877e52229bf3d9dc6c
v.mozharovsky <v.mozharovsky@gmail.com>
2018-03-10 17:03:30 +0200

    users added

--- src/app/shared/constants/users.ts
+++ src/app/shared/constants/users.ts
@@ -2,4 +2,7 @@ export const USERS = [
   {name: 'Alex', id: '9tqt1ffqfuft2uv1lo0ugsjjkc@group.calendar.google.com'},
   {name: 'Vitto', id: 'rml011muh7notp6khehcrsttjc@group.calendar.google.com'},
   {name: 'Nikita', id: 'fq09ub8np9crc0emp3c7i4obps@group.calendar.google.com'},
+  {name: 'Common', id: 'fnh6kgqiqego15vj2egmd7q2go@group.calendar.google.com'},
+  {name: 'test', id: 'creativejusticehere@gmail.com'},
 ]
+g
