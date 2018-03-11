f5ed159216114987a8fd06387f2b82e0b88bd0dd
v.mozharovsky <v.mozharovsky@gmail.com>
2018-03-11 16:27:57 +0200

    login button authorization

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Store} from "@ngrx/store"
+import * as authActions from "../../store/auth/auth.actions"
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( private store: Store<any>) { }
 
   ngOnInit() {
+  }
+
+  onLoginClick(){
+    this.store.dispatch(new authActions.TryLogin())
   }
 
 }
