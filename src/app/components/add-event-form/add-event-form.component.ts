b384ea9f138d5458f33b44f9184cb654d00af328
v.mozharovsky <v.mozharovsky@gmail.com>
2018-03-12 15:37:38 +0200

    form in progress

--- src/app/components/add-event-form/add-event-form.component.ts
+++ src/app/components/add-event-form/add-event-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms"
+import {Store} from "@ngrx/store"
+import {selectAllUsers} from "../../store/users/users.selectors"
+import {Observable} from "rxjs/Observable"
+import {IUser} from "../../shared/interfaces/users.interfaces"
 
 @Component({
   selector: 'app-add-event-form',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventFormComponent implements OnInit {
 
-  constructor() { }
+  users$: Observable<IUser[]>
+
+  constructor(private fb: FormBuilder, private store: Store<any>) {
+
+  }
 
   ngOnInit() {
+    this.addEventForm = this.fb.group({
+      usersSelect: [[''], Validators.required],
+      subject: ['', Validators.required]
+    });
+
+    this.users$ = this.store.select(selectAllUsers)
   }
+
+
+  name:string;
+  addEventForm: FormGroup;
+
+  items = [
+    {name: 'One', value: 1},
+    {name: 'Two', value: 2},
+    {name: 'Three', value: 3}
+  ];
+
+  onFormSubmit(){
+    debugger
+    this.addEventForm
+  }
+
 
 }
---
+++ src/app/shared/models/newEvent.model.ts
@@ -0,0 +1,39 @@
+import {ICalendarEvent} from "../interfaces/calendar.interfaces"
+
+const PRIMARY_COLOR = '#3ee0c666'
+const SECONDARY_COLOR = '#3ee0c666'
+
+export class CalendarEvent implements ICalendarEvent {
+  public id: string
+  public start: Date
+  public end: Date
+  public title: string
+  public color: {
+    primary: string,
+    secondary: string
+  }
+  public resizable: {
+    beforeStart: boolean,
+    afterEnd: boolean
+  }
+  public draggable: boolean
+
+  constructor(event) {
+    this.id = event.id
+    this.start = new Date(event.start.dateTime)
+    this.end = new Date(event.end.dateTime)
+    this.title = event.summary
+
+    this.color = {
+      primary: PRIMARY_COLOR,
+      secondary: SECONDARY_COLOR
+    }
+
+    this.resizable = {
+      beforeStart: true,
+      afterEnd: true,
+    }
+
+    this.draggable = true
+  }
+}
