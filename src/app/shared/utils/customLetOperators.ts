733fc3298e3758e182c2bf6aeb3bd4b02c771f4e
v.mozharovsky <v.mozharovsky@gmail.com>
2018-04-18 02:00:20 +0300

    backToAngularZone operator changed

--- src/app/shared/utils/customLetOperators.ts
+++ src/app/shared/utils/customLetOperators.ts
@@ -1,11 +1,18 @@
 import {Observable} from "rxjs/Observable"
 import {NgZone} from "@angular/core"
-import "@ngrx/core/add/operator/enterZone"
+import {catchError, map, tap} from "rxjs/operators"
+import {of} from "rxjs/observable/of"
 
-export function backToAngularZone<T>(zone: NgZone): (source: Observable<T>) => Observable<T> {
 
-  return (source: Observable<T>) => source.map(v => v).enterZone(zone)
+    import "@ngrx/core/add/operator/enterZone"
 
-}
+    export function backToAngularZone<T>(zone: NgZone): (source: Observable<T>) => Observable<T> {
+      return (source: Observable<T>) => source.enterZone(zone)
+        .pipe(
+          tap(() => console.log('We are back in Angular zone')),
+          // map(() => {...}),
+          // catchError(error => of(null))
+        )
+    }
 
 
